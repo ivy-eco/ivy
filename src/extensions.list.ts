0feb41c177d816b6880f539e11a31522bd6753bd
AFelipeCR <afeliperosas@gmail.com>
2026-07-25 09:38:19 -0700

    no extensions

--- src/extensions.list.ts
+++ src/extensions.list.ts
@@ -1,8 +1,5 @@
-import { InfoExtension } from '@ivy-eco/info';
+
 import { ExtensionDefinition } from './modules/extension/extension.interface';
-import { AternosExtension } from '@ivy-eco/aternos';
 
 export const extensionsList: ExtensionDefinition[] = [
-    { module: InfoExtension },
-    { module: AternosExtension }
 ];
